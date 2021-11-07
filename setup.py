f401e11a411eb330dd8e8f86fc272f093143c600
abcdef <mattkerschbaum@Hotmail.com>
2021-11-07 06:29:42 -0800

    Initial commit. No certificates included. Refer to certificate.py on how to create them

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
-setuptools.setup(name='goopycertificate',
+setuptools.setup(name='goopy_certificate',
 version='1.0',
 description='Goo configuration module based on configObj library',
 url='#',
-author='max',
+author='goo',
 #install_requires=[''],
 author_email='',
 packages=setuptools.find_packages(),
