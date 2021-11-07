a5545cf630e86d644c7e5db942edd8b27d059911
abcdef <mattkerschbaum@Hotmail.com>
2021-11-06 17:21:25 -0800

    Updated some test certificates for unit testing

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='goopycertificate',
+version='1.0',
+description='Goo configuration module based on configObj library',
+url='#',
+author='max',
+#install_requires=[''],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
