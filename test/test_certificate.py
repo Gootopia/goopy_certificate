5422081227ccfac4f718b4701691e8561ed7063a
abcdef <mattkerschbaum@Hotmail.com>
2021-11-11 05:23:14 -0800

    Added some comments on how to create certificates and install openSSH

--- test/test_certificate.py
+++ test/test_certificate.py
@@ -1,8 +1,22 @@
-# test_certificate.py
-
-from certificate import Certificate, CertificateError
+from goopy_certificate.certificate import Certificate, CertificateError
 from pathlib import Path
 
+"""
+Tests assume the presence of a couple of certificates stored in this folder (same level as this file).
+There doesn't need to be anything special about them other than one is private and one is public.
+Certificates are not currently pushed to the git-repo, so they need to be created prior to running the tests
+
+Info on installing OpenSSL: https://www.thesecmaster.com/procedure-to-install-openssl-on-the-windows-platform/
+
+Info on creating certificates can be found:
+
+    See: https://talkdotnet.wordpress.com/2019/08/07/generating-a-pem-private-and-public-certificate-with-openssl-on-windows/
+    also see: https://adamtheautomator.com/openssl-windows-10/
+    Perform the following in the same directory as this .py module
+    1) openssl req -x509 -newkey rsa:4096 -keyout {keyname}.pem -out {publickey_name}.pem -nodes
+    2) openssl x509 -outform der -in {publickey_name}.pem -out {publickey_name}.crt
+
+"""
 
 def test_get_certificate_bad_path():
     """ Fail when bad path/filename"""
